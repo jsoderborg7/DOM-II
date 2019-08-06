7bef2e583700ec2102f76e2c5944538d90d3fca3
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-06 14:29:58 -0600

    mouseover and mouseout working on all nav items, added dblclick to logo

--- js/index.js
+++ js/index.js
@@ -1,13 +1,25 @@
-const navLink = document.querySelector('a');
+// nav link styles
 
-navLink.addEventListener("mouseover", event =>{
-  navLink.style.color = "purple";
-  navLink.style.fontSize = "4rem";
-  // event.stopPropagation();
+const navLink = document.querySelectorAll('.nav-link');
+
+navLink.forEach((function(event) {
+  event.addEventListener("mouseover", function(){
+    event.style.color = "purple";
+    event.style.fontSize = "4rem";
+    event.stopPropagation();
+})
+  event.addEventListener("mouseout", function(){
+    event.style.color = "black";
+    event.style.fontSize= "1.7rem"; 
+    event.stopPropagation();
+  })
+}))
+
+// Logo styles
+
+const logo = document.querySelector('.logo-heading');
+
+logo.addEventListener("dblclick", event =>{
+  logo.style.transform = "rotate(180deg)";
 })
 
-navLink.addEventListener("mouseout", event =>{
-  navLink.style.color = "black";
-  navLink.style.fontSize= "1.7rem";
-  // event.stopPropagation();
-})
