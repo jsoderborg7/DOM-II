7048a72c8337e1791f1faa3deb4a35ac83a4b8a1
Jenn Soderborg <jsoderborg7@gmail.com>
2019-08-06 13:16:46 -0600

    working on nav link transformation, have it on home, but no others

--- js/index.js
+++ js/index.js
@@ -1,1 +1,13 @@
-// Your code goes here
+const navLink = document.querySelector('a');
+
+navLink.addEventListener("mouseover", event =>{
+  navLink.style.color = "purple";
+  navLink.style.fontSize = "4rem";
+  // event.stopPropagation();
+})
+
+navLink.addEventListener("mouseout", event =>{
+  navLink.style.color = "black";
+  navLink.style.fontSize= "1.7rem";
+  // event.stopPropagation();
+})
